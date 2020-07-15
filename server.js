5fae680a0ffb6f878a61e18e8bb8dbc9680316e2
Pradhay <Pradhay@Pradhays-MacBook-Pro.local>
2020-07-15 23:33:57 +0100

    make it better

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.static('public'));
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
- 
-var server = app.listen(80, function(){
+const PORT = process.env.PORT || 3000;
+var server = app.listen(PORT, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
